5a2af63544cac78c9d9186b42738f9df2ec81c6b
folospior <folosp2@gmail.com>
2024-08-25 01:53:10 +0200

    initial commit

---
+++ editor_script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+date=$(date)
+file_location=$HOME/Pictures/screenshot+${date}_grim.png
+title="Do you wish to edit your screenshot?"
+
+slurp | grim -g - - | wl-copy && wl-paste > $file_location
+
+output=$(printf "No\nYes" | wofi -d -p "${title}")
+
+case $output in
+	"No")
+		exit 0;
+		;;
+	"Yes")
+		gimp "${file_location}"
+		;;
+esac
