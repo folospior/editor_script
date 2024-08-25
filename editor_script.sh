652237b40e93b1f1e6496a6ee97e652cb03b9a1d
folospior <folosp2@gmail.com>
2024-08-25 13:21:55 +0200

    exit if slurp fails

--- editor_script.sh
+++ editor_script.sh
@@ -4,7 +4,13 @@ date=$(date)
 file_location=$HOME/Pictures/screenshot+${date}_grim.png
 title="Do you wish to edit your screenshot?"
 
-slurp | grim -g - - | wl-copy && wl-paste > $file_location
+geometry=$(slurp)
+
+if [ $? -ne 0 ]; then
+	exit $?	
+fi
+
+grim -g "$geometry" - | wl-copy && wl-paste > $file_location
 
 output=$(printf "No\nYes" | wofi -d -p "${title}")
 
